beda2fcd3e2bd29550a6c63e219eeb2c0b509b17
czlll <1781750070@qq.com>
2020-07-09 16:15:04 +0800

    '增加仓储部分的domain'

---
+++ src/back-end/src/main/java/com/tcsquad/ilogistics/domain/storage/AdjustFrom.java
@@ -0,0 +1,62 @@
+package com.tcsquad.ilogistics.domain.storage;
+
+/**
+ * 
+ */
+public class AdjustFrom {
+    private long adjustId;           //调货单编号
+    private Item item;               //调货商品
+    private int itemNum;             //调货数量
+    private String toMainSiteId;     //处理调货主站编号
+    private String fromMainSiteId;   //请求调货主站编号
+    private String adjustStatus;     //调货单处理状态
+
+
+    public long getAdjustId() {
+        return adjustId;
+    }
+
+    public void setAdjustId(long adjustId) {
+        this.adjustId = adjustId;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getItemNum() {
+        return itemNum;
+    }
+
+    public void setItemNum(int itemNum) {
+        this.itemNum = itemNum;
+    }
+
+    public String getToMainSiteId() {
+        return toMainSiteId;
+    }
+
+    public void setToMainSiteId(String toMainSiteId) {
+        this.toMainSiteId = toMainSiteId;
+    }
+
+    public String getFromMainSiteId() {
+        return fromMainSiteId;
+    }
+
+    public void setFromMainSiteId(String fromMainSiteId) {
+        this.fromMainSiteId = fromMainSiteId;
+    }
+
+    public String getAdjustStatus() {
+        return adjustStatus;
+    }
+
+    public void setAdjustStatus(String adjustStatus) {
+        this.adjustStatus = adjustStatus;
+    }
+}
---
+++ src/back-end/src/main/java/com/tcsquad/ilogistics/domain/storage/MainSite.java
@@ -0,0 +1,43 @@
+package com.tcsquad.ilogistics.domain.storage;
+
+/**
+ * 
+ */
+public class MainSite {
+    private String mainsiteId;
+    private String province;
+    private String city;
+    private String district;
+
+    public String getMainsiteId() {
+        return mainsiteId;
+    }
+
+    public void setMainsiteId(String mainsiteId) {
+        this.mainsiteId = mainsiteId;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+}
