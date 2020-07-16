cc49cc96fdac83239aa9843a8799cd7f614505c5
czlll <1781750070@qq.com>
2020-07-17 00:57:35 +0800

    '新增逻辑库存相关类与接口'

---
+++ src/back-end/src/main/java/com/tcsquad/ilogistics/domain/storage/LogicInventory.java
@@ -0,0 +1,10 @@
+package com.tcsquad.ilogistics.domain.storage;
+
+import lombok.Data;
+
+@Data
+public class LogicInventory {
+    String mainsiteId;
+    String itemId;
+    Integer logicInventory;
+}
